93de81bb7c9d39d511f36304b90f2054860b6ea8
oAmorim2k5 <victorhuski@gmail.com>
2025-06-04 18:32:51 -0300

    Terceiro Cenario finalizado - Submitting an image and updating the list

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,4 +1,5 @@
 import assert from 'assert'
+import { parse } from 'path';
 
 class RegisterForm {
   elements = {
@@ -29,6 +30,11 @@ const colors = {
   errors: 'rgb(220, 53, 69)',
   success: 'rgb(25, 135, 84)'
 }
+const input = {
+      title: 'Alien BR',
+      url: 'https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg'
+}
+
 describe('Image Registration', () => {
   describe('Submitting an image with invalid inputs', () => {
     after(() => {
@@ -70,10 +76,9 @@ describe('Image Registration', () => {
 
   }) 
   describe('Submitting an image with valid inputs using enter key', () => {
-    const input = {
-      title: 'Alien BR',
-      url: 'https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg'
-    }
+    after(() => {
+      cy.clearAllLocalStorage()
+    })
 
     it('Given I am on the image registration page', () => {
       cy.visit('/')
@@ -124,6 +129,45 @@ describe('Image Registration', () => {
     })
 
     it("Then The inputs should be cleared", () => {
+      registerForm.elements.titleInput().should("have.value", "")
+      registerForm.elements.imageUrlInput().should("have.value", "")
+    })
+  })
+  describe('Submitting an image and updating the list', () => {
+    
+    it('Given I am on the image registration page', () => {
+      cy.visit('/')
+    })
+
+    it(`Then I have entered "${input.title}" in the title field`, () => {
+      registerForm.typeTitle(input.title)
+    })
+
+    it(`Then I have entered "${input.url}" in the URL field`, () => {
+      registerForm.typeUrl(input.url)
+    })
+
+    it('When I click the submit button', () => {
+      registerForm.elements.submitBtn().click()
+    })
+
+    it('And the list of registered images should be updated with the new item', () => {
+      cy.contains(input.title)
+    })
+
+    it('And the new item should be stored in the localStorage', () => {
+      cy.window().then((window) => {
+        const storedData = window.localStorage.getItem("tdd-ew-db")
+        expect(storedData).to.not.be.null
+
+        const parsedData = JSON.parse(storedData)
+        const imageExists = parsedData.some(item => item.imageUrl == input.url)
+
+        expect(imageExists).to.be.true
+      })
+    })
+
+    it('Then The inputs should be cleared', () => {
       registerForm.elements.titleInput().should("have.value", "")
       registerForm.elements.imageUrlInput().should("have.value", "")
     })
